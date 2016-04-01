dfa427f13799db32f807955ba89eb1199211b27d
Gayane Gasparyan <gayane.gasparyan@softconstruct.local>
2016-04-01 14:51:37 +0400

    start

---
+++ js/components/active_user.js
@@ -0,0 +1,13 @@
+/**
+ * Created by gayane.gasparyan on 01/04/2016.
+ */
+import React from 'react';
+
+const ActiveUser = (props) => {
+    console.log(props.activeUser);
+    return (
+    <div>a</div>
+    );
+};
+
+export default ActiveUser;
